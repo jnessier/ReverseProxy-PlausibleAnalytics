68d1ef9b307bd8f8f8c3dd8381c494b58818d3f1
Jonathan Nessier <jonathan.nessier@neoflow.ch>
2021-09-01 08:17:43 +0200

    Justified a few details

--- options.php
+++ options.php
@@ -1,7 +1,7 @@
 <?php
 
 // Set the relative URI when the reverse proxy is running in a folder.
-// If your proxy runs under https://example.com/stats, then set /stats as relative URI and otherwise just leave it blank.
+// If your proxy runs under https://example.com/stats, then set /stats as relative URI and otherwise just leave the string blank.
 $relativeUri = '/stats';
 
 // Set all allowed URI which should be accessible trough the proxy
@@ -11,9 +11,10 @@ $whitelist = [
     '/api/event'
 ];
 
-// Optional, map allowed URI to another for sanitizing any proofs of Plausible Analytics in the URI
+// Optional, map allowed URI to another for sanitizing any proofs of Plausible Analytics in the URI.
+// If not needed then leave the array empty.
 $mapping = [
-    '/js/script.js' => '/js/plausible.outbound-links.js'
+    '/foo/bar.js' => '/js/plausible.outbound-links.js'
 ];
 
 // Set URL of Plausible Analytics
